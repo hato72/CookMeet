616f8fef8f764bac0df73567c6c93123808c9a6a
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-24 16:27:10 +0900

    add : TextInputコンポーネントを作成

---
+++ src/components/ui/form/TextInput.tsx
@@ -0,0 +1,40 @@
+import { TextField } from "@mui/material";
+import React from "react";
+
+type TextInputProps = {
+    label: string;
+    type?: React.HTMLInputTypeAttribute
+    helperText: string;
+    placeholder: string;
+    value: string;
+    error: boolean;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+/**
+ * ### メールアドレスやパスワードの入力フォームを提供するコンポーネント
+ * 
+ * @param label フォームのラベル
+ * @param helperText バリデーションエラー時のメッセージ
+ * @param type (optional) フォームのタイプ(デフォルトは'text')
+ * @param placeholder フォームのプレースホルダー
+ * @param value フォームの値
+ * @param error バリデーションエラーの有無
+ * @param onChange フォームの値が変更された時のコールバック
+ */
+const TextInput: React.FC<TextInputProps> = ({ label, helperText, placeholder, value, type = 'text', error, onChange }) => {
+    return (
+        <TextField
+        variant="outlined"
+        label={label}
+        helperText={error ? helperText : ''}
+        error={error}
+        placeholder={placeholder}
+        value={value}
+        type={type}
+        onChange={onChange}
+        />
+    );
+};
+
+export default TextInput;
