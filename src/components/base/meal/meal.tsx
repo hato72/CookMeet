28ed0017b7997e634a69f738a7878925bfdaa9e9
Haruki65 <u062016e@ecs.osaka-u.ac.jp>
2024-04-27 14:32:48 +0900

    生成結果ページの整理

--- src/components/base/meal/meal.tsx
+++ src/components/base/meal/meal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./meal.module.scss";
+import Link from "next/link";
 
 interface CardProps {
     img: string;
@@ -7,17 +8,25 @@ interface CardProps {
     description: string;
     width?: string;
     height?: string;
+    url: string;
 }
 
-const RecipeCard: React.FC<CardProps> = ({ img, title, description, width, height }) => {
+/* 表示可能なDescription文字数 */
+const MAX = 30;
+
+const RecipeCard: React.FC<CardProps> = ({ img, title, description, width, height, url }) => {
+    const shortDescription = description.length > MAX ? `${description.slice(0, MAX)}...` : description;
     return (
         <div className={`${styles.card} ${styles.defaultBackground}`} style={{ width: width, height: height }}>
             <div className={styles.card__body}>
                 <img src={img} className={styles.card__image} alt={title} />
                 <h2 className={`${styles.card__title} ${styles.defaultText}`}>{title}</h2>
-                <p className={`${styles.card__description} ${styles.defaultText}`}>{description}</p>
+                <p className={`${styles.card__description} ${styles.defaultText}`}>{shortDescription}</p>
             </div>
-            <button className={`${styles.card__btn} `}>View Recipe</button>
+            <Link href={url}>
+                <button className={`${styles.card__btn} `}>レシピを見る</button>
+            </Link>
+
         </div>
     );
 }
