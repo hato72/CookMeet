b6f35f38df69b76b6d5dacde5069c72c08438345
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-04-28 09:41:28 +0900

    add : Questionの残り数を示すカウンタを追加

--- src/app/cook/question/page.tsx
+++ src/app/cook/question/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import BlackRoundButton from "@/components/ui/buttun/BlackRoundButton";
@@ -95,9 +95,13 @@ export default function Questions() {
                 </div>) : (
                 <div className={styles.questionCard}>
                     {/* Current Question  */}
-                    <h2>
-                        質問: {currentQuestion + 1} / {questions.length}
-                    </h2>
+                    <ul className={styles.questionCounter_list}>
+                        {questions.map((_, index) => (
+                            <li key={index} className={styles.questionCounter_item}>
+                                <div className={styles.questionCounter}></div>
+                            </li>
+                        ))}
+                    </ul>
                     <h3 className={styles.question}>{questions[currentQuestion].text}</h3>
 
                     {/* List of possible answers  */}
