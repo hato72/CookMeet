f0192f7e6f75f120eed7ac28540ec62e8c6077a2
hato <hatotori72@gmail.com>
2024-05-15 16:30:34 +0900

    写真撮影ページデザイン変更

--- src/app/cook/photo/page.tsx
+++ src/app/cook/photo/page.tsx
@@ -55,31 +55,9 @@ const UploadPage: React.FC = () => {
                     <h1 className="text-3xl font-bold tracking-tight text-gray-900">記録しよう</h1>
             </div>
             
-            {/* <form className="upload-form" onSubmit={handleSubmit}>
-                <input
-                    type="file"
-                    name="photo"
-                    id="photo"
-                    className="upload-input"
-                    accept="image/*"
-                    onChange={handleFileChange}
-                />
-            </form>
-            <div className="comment-section">
-                <textarea
-                    className="comment-input"
-                    placeholder="コメントを入力してください"
-                    value={comment}
-                    onChange={handleCommentChange}
-                />
-            </div>
-            <Link href={'./repo'}>
-                <BlackRoundButton type="submit" className="submit-button">完了</BlackRoundButton>
-            </Link> */}
-
             {/* File upload section */}
-            <div className="Group7 w-[427px] h-[207.48px] left-[136px] top-[350px] absolute">
-                <div className="Rectangle1 w-[427px] h-[207px] left-0 top-0 absolute bg-sky-600/opacity-10 rounded-xl border-2 border-sky-600" />
+            <div className="w-[427px] h-[207.48px] left-[220px] top-[200px] absolute">
+                {/* <div className="Rectangle1 w-[427px] h-[207px] left-0 top-0 absolute bg-sky-600/opacity-10 rounded-xl border-2 border-sky-600" />
                 <div className="Group2 w-[160.40px] h-40 left-[134.03px] top-[47.52px] absolute">
                     <label htmlFor="photo" className="OrSelectFilesFromDevice w-[160.40px] h-[13.64px] left-0 top-[79.98px] absolute text-center text-white/opacity-30 text-2xl font-normal font-['SF Pro']">
                         写真アップロード
@@ -92,12 +70,26 @@ const UploadPage: React.FC = () => {
                         accept="image/*"
                         onChange={handleFileChange}
                     />
-                </div>
+                </div> */}
+                <label className="flex flex-col items-center justify-center bg-[#f0f0f0] border-2 border-dashed border-[#0066CC] rounded-lg cursor-pointer w-[400px] h-[200px]">
+                    <i className="fa fa-cloud-upload-alt text-3xl text-[#0066CC]"></i>
+                    <img src="/cloudimage/cloud.jpeg" alt=""></img>
+                    
+                    <span className="font-roboto text-base text-[#121212] mt-2">
+                        写真アップロード
+                    </span>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={handleFileChange}
+                    />
+                </label>
             </div>
 
             {/* Comment section */}
-            <div className="left-[763px] top-[349px] absolute text-black/opacity-60 text-lg font-normal font-['Roboto']">コメント</div>
-            <div className="Rectangle7 w-[554px] h-[453px] left-[738px] top-[336px] absolute bg-white border border-stone-300">
+            <div className="left-[763px] top-[200px] absolute text-black/opacity-60 text-lg font-normal font-['Roboto']">コメント</div>
+            <div className="Rectangle7 w-[554px] h-[350px] left-[738px] top-[200px] absolute bg-white border border-stone-300">
                 <textarea
                     className="comment-input w-full h-full p-4"
                     placeholder="コメントを入力してください"
@@ -108,7 +100,7 @@ const UploadPage: React.FC = () => {
 
             {/* Submit button */}
             <Link href={'./repo'}>
-                <BlackRoundButton type="button" className="submit-button absolute left-[600px] top-[850px]" onClick={handleSubmit}>
+                <BlackRoundButton type="button" className="submit-button absolute left-[650px] top-[600px]" onClick={handleSubmit}>
                     完了
                 </BlackRoundButton>
             </Link>
@@ -118,3 +110,5 @@ const UploadPage: React.FC = () => {
 };
 
 export default UploadPage;
+
+//どっちも同じくらい
